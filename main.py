a873316c8701b009d3fd0c0aaa452146c3643bf0
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-03-22 16:03:47 -0500

    Update iata data

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from models.flight_deals import FlightDeals
 import dotenv
 import os
 import requests
-from pprint import pprint
-import json
 
 dotenv.load_dotenv()
 
@@ -13,30 +11,43 @@ TWILIO_SID = os.getenv('TWILIO_SID')
 TWILIO_TOKEN = os.getenv('TWILIO_TOKEN')
 TWILIO_PHONE = os.getenv('TWILIO_PHONE')
 KIWI_TOKEN = os.getenv('KIWI_TOKEN')
-
+KIWI_ENDPOINT = 'https://tequila-api.kiwi.com'
 sheety_headers = {'Authorization': SHEETY_TOKEN}
 
 
 def get_sheety_data():
-    res = requests.get(
-        SHEETY_ENDPOINT,
-        headers=sheety_headers,
-    )
+    res = requests.get(SHEETY_ENDPOINT, headers=sheety_headers,)
     res = res.json()['prices']
-
     data = [FlightDeals(city=i['city'], iataCode=i['iataCode'],
                         id=i['id'], lowestPrice=i['lowestPrice'], ) for i in res]
     return data
 
 
-sheety_data = get_sheety_data()
-for e in sheety_data:
-    element_json = e.to_json()
-    endpoint = f'{SHEETY_ENDPOINT}/{e.id}'
-    res = requests.put(
+def get_iata_data(data: FlightDeals):
+    endpoint = f'{KIWI_ENDPOINT}/locations/query'
+    res = requests.get(
         endpoint,
-        json={'price': element_json},
-        headers={**sheety_headers, 'Content-Type': 'application/json'},
+        headers={'apikey': KIWI_TOKEN},
+        params={'location_types': 'city', 'term': data.city}
     )
-    print(res.json())
-    print(res.status_code)
+    return res.json()['locations'][0]['code']
+
+
+sheety_data = get_sheety_data()
+
+
+def update_iata():
+    for e in sheety_data:
+        iata = get_iata_data(e)
+        e.iataCode = iata
+        element_json = e.to_json()
+        endpoint = f'{SHEETY_ENDPOINT}/{e.id}'
+        requests.put(
+            endpoint,
+            json={'price': element_json},
+            headers={**sheety_headers, 'Content-Type': 'application/json'},
+        )
+    print(sheety_data)
+
+
+update_iata()
--- models/flight_deals.py
+++ models/flight_deals.py
@@ -4,10 +4,7 @@ class FlightDeals:
         self.city = city
         self.id = id
         self.lowestPrice = lowestPrice
-        if iataCode == '':
-            self.iataCode = 'TESTING'
-        else:
-            self.iataCode = iataCode
+        self.iataCode = iataCode
 
     def __str__(self):
         return f'city: {self.city} iataCode: {self.iataCode} id: {self.id} lowestPrice: {self.lowestPrice}'
